b76fc8ebd556a950c9293a070826fc1371738b9a
Пелько Андрей Алексеевич <pelpro@yandex.ru>
2020-05-05 16:50:26 +0300

    Edit code-style for hello_world

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
-using std::string;
-using std::cout;
 using std::cin;
+using std::cout;
+using std::string;
 
-int main(){
-std::string name;//initialise name
-std::cin>>name;//cin name
-std::cout<<"Hello World from "<<name;//write hello world @name
-return 0;
+int
+main()
+{
+  std::string name;                         // initialise name
+  std::cin >> name;                         // cin name
+  std::cout << "Hello World from " << name; // write hello world @name
+  return 0;
 }
