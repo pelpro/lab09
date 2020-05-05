401257499585d601264e7bc26854eeb479de447f
Andrew <32572000+pelpro@users.noreply.github.com>
2020-05-05 16:53:18 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,8 +5,8 @@ using std::cout;
 using std::cin;
 
 int main(){
-std::string name;//initialise name
-std::cin>>name;//cin name
-std::cout<<"Hello World from "<<name;//write hello world @name
+std::string name;//обьявляем name
+std::cin>>name;//считываем name
+std::cout<<"Hello World from "<<name;//пишем hello world @name
 return 0;
 }
