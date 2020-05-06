81fe5640132acbcae36a49f38e7938a171451108
Пелько Андрей Алексеевич <pelpro@yandex.ru>
2020-05-06 03:16:23 +0300

    All

---
+++ tests/test_account.cpp
@@ -0,0 +1,29 @@
+#include "Account.h"
+
+#include <gtest/gtest.h>
+
+TEST(Account, AccountTestAll)
+{
+	Account first(1, 1000);
+	
+	EXPECT_EQ(first.id(), 1);
+	EXPECT_EQ(first.GetBalance(), 1000);
+	EXPECT_THROW(first.ChangeBalance(100), std::runtime_error);
+	EXPECT_EQ(first.id(), 1);	
+	EXPECT_EQ(first.GetBalance(), 1000);
+
+	EXPECT_NO_THROW(first.Lock());
+	EXPECT_THROW(first.Lock(), std::runtime_error);
+	EXPECT_EQ(first.GetBalance(), 1000);
+	EXPECT_NO_THROW(first.ChangeBalance(500));
+	EXPECT_EQ(first.GetBalance(), 1500);
+
+	EXPECT_EQ(first.id(), 1);
+
+	EXPECT_NO_THROW(dimka.Unlock());
+	EXPECT_NO_THROW(dimka.Unlock());
+	EXPECT_EQ(first.id(), 1);
+	EXPECT_EQ(first.GetBalance(), 1500);
+	EXPECT_THROW(first.ChangeBalance(500), std::runtime_error);
+}
+
---
+++ tests/test_transaction.cpp
@@ -0,0 +1,82 @@
+#include "Transaction.h"
+#include "Account.h"
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+using::testing::AtLeast;
+using::testing::DoAll;
+using::testing::Return;
+
+class AccountMock: public Account {
+public:
+	MockAccount(int id, int sum) : Account(id, sum) {}
+        MOCK_METHOD0(id, int());
+	MOCK_METHOD0(GetBalance, int());
+	MOCK_METHOD1(ChangeBalance, void(int diff));
+	MOCK_METHOD0(Lock, void());
+	MOCK_METHOD0(Unlock, void());
+};
+
+TEST(Transaction, TransactionMock)
+{
+        MockAccount first(1, 1000), second(2, 1000);
+        Transaction transaction;
+
+        EXPECT_CALL(first, Lock())
+                .Times(1);
+        EXPECT_CALL(second, Lock())
+                .Times(1);
+        EXPECT_CALL(second, ChangeBalance(100))
+                .Times(1);
+        EXPECT_CALL(first, ChangeBalance(-100))
+                .Times(1);
+        EXPECT_CALL(firsy, Unlock())
+                .Times(1);
+        EXPECT_CALL(second, Unlock())
+                .Times(1);
+
+        transaction.Make(first, second, 100);
+}
+
+TEST(Transaction, TransactionInvalid)
+{
+        MockAccount first(1, 1000), second(2, 1000);
+        Transaction transaction;
+        EXPECT_THROW(transaction.Make(first, first, 150), std::logic_error);
+}
+
+TEST(Transaction, TransactionNegative)
+{
+        MockAccount first(1, 1000), second(2, 1000);
+        Transaction transaction;
+        EXPECT_THROW(transaction.Make(first, second, -1), std::invalid_argument);
+}
+
+TEST(Transaction, TransactionSmall)
+{
+        MockAccount first(1, 1000), second(2, 1000);
+        Transaction transaction;
+        EXPECT_THROW(transaction.Make(first, second, 10), std::logic_error);
+}
+
+TEST(Transaction, TransactionEnough)
+{
+        Account first(1, 1000), second(2, 1000);
+        Transaction transaction;
+        transaction.Make(first, second, 100);
+        EXPECT_EQ(first.GetBalance(), 900);
+        EXPECT_EQ(second.GetBalance(), 1100);
+}
+
+TEST(Transaction, TransactionNotEnoughFunds)
+{
+        Account first(1, 1000), second(2, 1000);
+        Transaction transaction;
+        transaction.Make(first, second, 1500);
+
+	EXPECT_EQ(petya.GetBalance(), 1000);
+        EXPECT_EQ(vasya.GetBalance(), 1000);
+
+}
+
